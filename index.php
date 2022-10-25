dd824423183104320f8be040f73baf7d362f31b4
Gianluca Starke <gianlucastarke@gmail.com>
2022-10-24 23:24:22 -0300

    sim

--- index.php
+++ index.php
@@ -2,7 +2,22 @@
 
 /**
  * Aqui vamos escrever tudo que queremos que inicie logo que o site abrir.
- * No caso, será o App.php.
+ * No caso, será o autoloader.php e o App.php.
  */
+
+require_once "dist/php/controllers/core/autoloader.php";
+require_once "dist/php/controllers/core/App.php";
+
+use dist\php\controllers\core\App;
+
+try{
+    $router = new App();
+}
+catch(Error $e){
+    print_r($e->getMessage());
+}
+catch(Exception $e){
+    print_r($e->getMessage());
+}
 
 ?>
