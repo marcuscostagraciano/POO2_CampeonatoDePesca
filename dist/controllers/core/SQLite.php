9eac62503386f675807052f171c242693cbb574b
Gianluca Starke <gianlucastarke@gmail.com>
2022-11-27 16:29:20 -0300

    Criar classe que controla a conexão com o banco

--- dist/controllers/core/SQLite.php
+++ dist/controllers/core/SQLite.php
@@ -2,12 +2,33 @@
 
 namespace dist\controllers\core;
 
+use Exception;
 use PDO;
 
 class SQLite {
     private $conexao;
 
-    public function 
+    public function __construct(){
+        $this->conexao = new PDO("sqlite:/src/sqlite/pesca.db"); // Cria conexao com o banco
+    }
+
+    public function query(string $sql, array $parametros, bool $fetchAll = true, bool $debug = false): array{
+        $query = $this->conexao->prepare($sql); // Prepara o sql
+
+        // Verifica se houve erros na execução.
+        // Se $debug for verdadeiro ele retorna a mensagem de erro do SQL.
+        // Se for falso ele retorna "Houve um erro ao acessar o banco.".
+        if(!($query->execute($parametros))) throw new Exception(
+            $debug
+            ? join(" :: ", $query->errorInfo())
+            : "Houve um erro ao acessar o banco."
+        );
+
+        // Retorna o(s) resultado(s) do sql executado
+        return $fetchAll
+        ? $query->fetchAll(PDO::FETCH_ASSOC)
+        : $query->fetch(PDO::FETCH_ASSOC);
+    }
 }
 
 ?>
